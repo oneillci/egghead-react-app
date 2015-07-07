f84ae8e1aaa53d8908d3e9fac9defa692fff9b35
Ciaran O'Neill <oneillci@gmail.com>
2015-07-07 11:38:48 +0100

    updated home & main to use es6

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,22 +3,37 @@
 var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
 
-var Main = React.createClass({
-	render: function(){
-		return (
-			<div className="main-container">
+// var Main = React.createClass({
+// 	render: function(){
+// 		return (
+// 			<div className="main-container">
+// 				<nav className="navbar navbar-default">
+// 					<div className="col-sm-7 col-sm-offset-2" style={{marginTop:15}}>
+// 						MENU
+// 					</div>
+// 				</nav>
+// 				<div className="container">
+// 					<RouteHandler />
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// });
+
+class Main extends React.Component {
+	render() {
+		return (<div className="main-container">
 				<nav className="navbar navbar-default">
 					<div className="col-sm-7 col-sm-offset-2" style={{marginTop:15}}>
-						MENU
+						es6 menu
 					</div>
 				</nav>
 				<div className="container">
 					<RouteHandler />
 				</div>
-			</div>
-		)
+			</div>)
 	}
-});
+}
 
 // react-router now responsible for rendering, so just export main
 // React.render(<Main />, document.getElementById("app"));
--- app/components/home.js
+++ app/components/home.js
@@ -1,13 +1,19 @@
 var React = require("react");
 
-var Home = React.createClass({
-	render: function() {
-		return(
-			<h2 className="text-center">
-				Search by github username
-			</h2>
-		)
+// var Home = React.createClass({
+// 	render: function() {
+// 		return(
+// 			<h2 className="text-center">
+// 				Search by github username
+// 			</h2>
+// 		)
+// 	}
+// });
+
+class Home extends React.Component {
+	render() {
+		return <h2 className="text-center">Search by asdfasdf</h2>
 	}
-});
+}
 
 module.exports = Home;
